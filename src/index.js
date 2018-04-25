43f0ad99b3cd414b3b20fc6eeed5560fcc399bcd
santinucera <santinucera4@gmail.com>
2018-04-25 15:49:43 -0300

    Initial commit

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import HomeContainer from './app/screens/Home/index.js';
-import registerServiceWorker from './registerServiceWorker';
-
-
-ReactDOM.render(<HomeContainer />, document.getElementById('root'));
-registerServiceWorker();
