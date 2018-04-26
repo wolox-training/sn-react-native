5be916cb00bda9d199959f0e30e298a5eaac4275
santinucera <santinucera4@gmail.com>
2018-04-26 18:04:56 -0300

    Added of pass information to book-detail

--- src/app/screens/BookDetail/components/BookAttr/index.js
+++ src/app/screens/BookDetail/components/BookAttr/index.js
@@ -1,17 +1,17 @@
 import React from "react"
 
 export class BookAttr extends React.Component{
-  
+
 
   render(){
     return(
       <div>
-        <img />
-        <h1></h1>
-        <h2></h2>
-        <h2></h2>
-        <h2></h2>
-        <h3></h3>
+        <img className="detail-image" src={this.props.book.image_url}/>
+        <h1>{this.props.book.title}</h1>
+        <h2>{this.props.book.author}</h2>
+        <h2>{this.props.book.year}</h2>
+        <h2>{this.props.book.genre}</h2>
+        <h3>{this.props.book.publisher}</h3>
         <button>Alquilar</button>
       </div>
     )
--- src/app/screens/BookDetail/index.js
+++ src/app/screens/BookDetail/index.js
@@ -1,23 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {BookAttr} from "./components/BookAttr/index.js"
 import {Suggestions} from "./components/Suggestions/index.js"
 import {NewComment} from "./components/NewComment/index.js"
 import {Comments} from "./components/Comments/index.js"
+import dataBooks from "../../../data/books.json";
 
 export class BookDetail extends React.Component {
-  state = {book:""}
+  state = {book: ""}
+
+  findBook(id){
+    let numId = parseInt(id)
+    return dataBooks.find(book => (book.id === numId))
+  }
 
   componentWillMount(){
-
+    this.setState({book: this.findBook(this.props.match.params.id)})
   }
 
   render(){
     return (
       <div>
-        <BookAttr />
-        <Suggestions />
-        <NewComment />
-        <Comments />
+        <BookAttr book={this.state.book}/>
+        <Suggestions book={this.state.book}/>
+        <NewComment book={this.state.book}/>
+        <Comments book={this.state.book}/>
       </div>
     )
   }
