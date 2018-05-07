e0c750b6e8b095c326374ef9bfa40718f9722b9c
santinucera <santinucera4@gmail.com>
2018-05-07 14:33:20 -0300

    Added service acount

--- src/app/screens/LogIn/index.js
+++ src/app/screens/LogIn/index.js
@@ -1,41 +1,35 @@
 import React from 'react';
-import axios from 'axios';
+
+import validateUser from '../../../service/account';
 
 import LogInLayout from './layout';
 import './styles.css';
 
 class LogIn extends React.Component {
-  validateLogIn = async () => {
-    document.getElementById('password-validation').style.display = 'none';
-    document.getElementById('user-validation').style.display = 'none';
+  validateLogIn = () => {
+    this.showErrorMessage('password-validation', 'none');
+    this.showErrorMessage('user-validation', 'none');
     const user = document.getElementById('user').value;
     const password = document.getElementById('password').value;
     let validationFlag = true;
     if (!this.validatePassword(password)) {
-      document.getElementById('password-validation').style.display = 'block';
+      this.showErrorMessage('password-validation', 'block');
       validationFlag = false;
     }
     if (!this.validateEmail(user)) {
-      document.getElementById('user-validation').style.display = 'block';
+      this.showErrorMessage('user-validation', 'block');
       validationFlag = false;
     }
-
     if (validationFlag) {
-      await axios
-        .post('https://wbooks-api-stage.herokuapp.com/api/v1/users/sessions', {
-          email: user,
-          password
-        })
-        .then(res => {
-          if (res.status !== 404 || res.status !== 401) {
-            window.location.href = '/dashboard';
-            sessionStorage.setItem('user', user);
-          }
-        })
-        .catch(e => (document.getElementById('api-validation').style.display = 'block'));
+      try {
+        validateUser(user, password);
+      } catch (e) {
+        this.showErrorMessage('api-validation', 'block');
+      }
     }
   };
 
+  showErrorMessage = (idError, display) => (document.getElementById(idError).style.display = display);
   validatePassword = password => password.length >= 8 && password.length <= 52;
   validateEmail = email => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(email);
 
---
+++ src/service/account.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+import { ROUTES } from '../app/config/routes';
+
+async function validateUser(user, password) {
+  await axios
+    .post('https://wbooks-api-stage.herokuapp.com/api/v1/users/sessions', {
+      email: user,
+      password
+    })
+    .then((window.location.href = ROUTES.HOME())(sessionStorage.setItem('user', user)));
+}
+
+export default validateUser;
