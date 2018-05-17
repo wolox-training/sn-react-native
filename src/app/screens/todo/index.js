0fdd1e414d007e5d523df876a95fed3cc0401c26
santinucera <santinucera4@gmail.com>
2018-05-17 18:24:04 -0300

    Fixing initial commit

--- src/app/screens/todo/index.js
+++ src/app/screens/todo/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { View, TextInput, ScrollView, Button } from 'react-native';
 import PropTypes from 'prop-types';
@@ -7,16 +7,14 @@ import { actionCreators } from '../../redux/todo/actions';
 
 import Todo from './component/todo';
 
-class TodoContainer extends Component {
-  render() {
-    return (
-      <View>
-        <TextInput onSubmitEditing={this.props.addTodo} placeholder="Enter ToDo" />
-        <ScrollView>{this.props.todos.map(todo => <Todo key={todo.id} todo={todo} />)}</ScrollView>
-        <Button onPress={this.props.removeSelectedTodo} title="Remove selected items" />
-      </View>
-    );
-  }
+function TodoContainer({ addTodo, todos, removeSelectedTodo }) {
+  return (
+    <View>
+      <TextInput onSubmitEditing={addTodo} placeholder="Enter ToDo" />
+      <ScrollView>{todos.map(todo => <Todo key={todo.id} todo={todo} />)}</ScrollView>
+      <Button onPress={removeSelectedTodo} title="Remove selected items" />
+    </View>
+  );
 }
 
 const mapDispatchToProps = dispatch => ({
