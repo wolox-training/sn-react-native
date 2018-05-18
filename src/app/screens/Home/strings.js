aa672aab179ef77eb72825b537a8d3bdfaa097e5
santinucera <santinucera4@gmail.com>
2018-05-18 14:22:25 -0300

    Initial commit

--- src/app/screens/Home/strings.js
+++
@@ -1,7 +0,0 @@
-const name = 'Nombre';
-const author = 'Autor';
-const selectFilter = 'Seleccionar filtro';
-const select = 'Seleccionar';
-const search = 'Buscar...';
-
-export { name, author, select, selectFilter, search };
--- src/app/screens/LogIn/constants.js
+++
@@ -1,7 +0,0 @@
-const userId = 'user';
-const passwordId = 'password';
-const passwordValidationId = 'password-validation';
-const userValidationId = 'user-validation';
-const apiValidationId = 'api-validation';
-
-export { userId, passwordId, passwordValidationId, userValidationId, apiValidationId };
